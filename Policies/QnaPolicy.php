a4d4b60e84382b58c1336609d70880c0c0ee5c77
koojunho <koojh@9cells.com>
2016-01-31 14:55:04 +0900

    패키지 분리 작업

---
+++ Policies/QnaPolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ModernPUG\Qna\Policies;
+
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class QnaPolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+    }
+
+    public function qnaEdit($user, $item)
+    {
+        return $user->id === $item->writer_id;
+    }
+}
