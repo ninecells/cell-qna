296cf2ba8d958ac51d49c70c0fb6c396a5e88f4e
koojunho <koojh@9cells.com>
2016-02-08 01:27:35 +0900

    존재하지 않는 문서 접근시 예외처리

--- Http/Controllers/QController.php
+++ Http/Controllers/QController.php
@@ -77,7 +77,12 @@ class QController extends Controller
             ->with('comments.votes')
             ->with('votes')
             ->find($q_id);
-        config(['title' => $q->title.' - modernpug.org']);
+
+        if (!$q) {
+            abort(404);
+        }
+
+        config(['title' => $q->title . ' - modernpug.org']);
         return view('mpug::qna.pages.item', ['q' => $q]);
     }
 }
