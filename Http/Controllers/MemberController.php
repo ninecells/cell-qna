a10eeaee9b0f88f1344ef41e925e82f96ffc91a9
koojunho <koojh@9cells.com>
2016-03-16 19:48:25 +0900

    Member tab partial view 를 include 시에 member_id 를 넘기게 변경
    - 내부적으로 $member 를 얻기위해 쿼리를 하는 형태로 변경
    - 인자로 넘어오지 않은 $member 를 사용하고 있었는데 대부분의 member tab 을 생성하는 view 가 $member 를 만들고 있어서 우연히 문제가 없던 것
    - @include 에서 'member_id' 라는 key 가 필요하다는 것을 사용자가 알고있어야 하는게 마음에 안들지만 아직 더 좋은 방법이 있는지 모르겠음

--- Http/Controllers/MemberController.php
+++ Http/Controllers/MemberController.php
@@ -2,6 +2,7 @@
 
 namespace ModernPUG\Qna\Http\Controllers;
 
+use DB;
 use ModernPUG\Qna\Models\Answer;
 use ModernPUG\Qna\Models\Comment;
 use ModernPUG\Qna\Models\Question;
@@ -10,6 +11,42 @@ class MemberController extends Controller
 {
     public function GET_member_qna_info($member_id)
     {
+        $grade_qs = DB::select('
+    SELECT
+      COALESCE(SUM(V.grade),0) grade
+    FROM questions Q
+    JOIN votes V
+    ON V.votable_id = Q.id
+    AND V.votable_type = ?
+    WHERE writer_id = ?
+    AND Q.deleted_at IS NULL
+    ', [Question::class, $member_id]);
+        $grade_qs = (sizeof($grade_qs) == 0 ? 0 : $grade_qs[0]->grade);
+
+        $grade_as = DB::select('
+    SELECT
+      COALESCE(SUM(V.grade),0) grade
+    FROM answers A
+    JOIN votes V
+    ON V.votable_id = A.id
+    AND V.votable_type = ?
+    WHERE writer_id = ?
+    AND A.deleted_at IS NULL
+    ', [Answer::class, $member_id]);
+        $grade_as = (sizeof($grade_as) == 0 ? 0 : $grade_as[0]->grade);
+
+        $grade_cs = DB::select('
+    SELECT
+      COALESCE(SUM(V.grade),0) grade
+    FROM comments C
+    JOIN votes V
+    ON V.votable_id = C.id
+    AND V.votable_type = ?
+    WHERE writer_id = ?
+    AND C.deleted_at IS NULL
+    ', [Comment::class, $member_id]);
+        $grade_cs = (sizeof($grade_cs) == 0 ? 0 : $grade_cs[0]->grade);
+
         $qs = Question::where('writer_id', $member_id)
             ->with('viewCounts')
             ->with('comments.writer')
@@ -21,7 +58,10 @@ class MemberController extends Controller
 
         return view('mpug::qna.pages.member_qna', [
             'member_id' => $member_id,
-            'qs' => $qs
+            'grade_qs' => $grade_qs,
+            'grade_as' => $grade_as,
+            'grade_cs' => $grade_cs,
+            'qs' => $qs,
         ]);
     }
 }
--- resources/views/qna/pages/member_qna.blade.php
+++ resources/views/qna/pages/member_qna.blade.php
@@ -1,56 +1,10 @@
-<?php
-
-use App\User;
-use ModernPUG\Qna\Models\Question;
-use ModernPUG\Qna\Models\Answer;
-use ModernPUG\Qna\Models\Comment;
-
-$member = User::find($member_id);
-
-$grade_qs = DB::select('
-    SELECT
-      COALESCE(SUM(V.grade),0) grade
-    FROM questions Q
-    JOIN votes V
-    ON V.votable_id = Q.id
-    AND V.votable_type = ?
-    WHERE writer_id = ?
-    AND Q.deleted_at IS NULL
-    ', [Question::class, $member_id]);
-$grade_qs = (sizeof($grade_qs) == 0 ? 0 : $grade_qs[0]->grade);
-
-$grade_as = DB::select('
-    SELECT
-      COALESCE(SUM(V.grade),0) grade
-    FROM answers A
-    JOIN votes V
-    ON V.votable_id = A.id
-    AND V.votable_type = ?
-    WHERE writer_id = ?
-    AND A.deleted_at IS NULL
-    ', [Answer::class, $member_id]);
-$grade_as = (sizeof($grade_as) == 0 ? 0 : $grade_as[0]->grade);
-
-$grade_cs = DB::select('
-    SELECT
-      COALESCE(SUM(V.grade),0) grade
-    FROM comments C
-    JOIN votes V
-    ON V.votable_id = C.id
-    AND V.votable_type = ?
-    WHERE writer_id = ?
-    AND C.deleted_at IS NULL
-    ', [Comment::class, $member_id]);
-$grade_cs = (sizeof($grade_cs) == 0 ? 0 : $grade_cs[0]->grade);
-
-?>
 @extends('ncells::jumbotron.app')
 
 @section('content')
 <div class="row">
     <div class="col-lg-12">
         <div>
-            @include('ncells::auth.parts.member_tab', ['member_id' => $member->id, 'tabitem_key' => 'qna'])
+            @include('ncells::auth.parts.member_tab', ['member_id' => $member_id, 'tabitem_key' => 'qna'])
             <div class="tab-content">
                 <br/>
                 <div role="tabpanel" class="tab-pane active">
